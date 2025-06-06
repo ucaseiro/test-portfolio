d489afb5895c388f247e3143f4c10afa349a537d
ucaseiro <ugo.caseiro@ensaama.net>
2025-06-06 23:03:54 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# DN1_p5.js
+fichier de départ pour P5.js
---
+++ js/sketch.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function () {
+    const $cards = $('.card');
+    let currentIndex = 0;
+    function updateCarousel() {
+      $cards.removeClass('left center right');
+  
+      const total = $cards.length;
+      const leftIndex = (currentIndex - 1 + total) % total;
+      const centerIndex = currentIndex % total;
+      const rightIndex = (currentIndex + 1) % total;
+  
+      $cards.eq(leftIndex).addClass('left');
+      $cards.eq(centerIndex).addClass('center');
+      $cards.eq(rightIndex).addClass('right');
+      if (currentIndex<9){
+        $('.big-number').text('0'+(currentIndex+1));
+      } 
+      else{
+        $('.big-number').text((currentIndex+1));
+      } 
+    }
+  
+    $('.arrow-right').click(function () {
+      currentIndex = (currentIndex + 1) % $cards.length;
+      updateCarousel();
+    });
+  
+    $('.arrow-left').click(function () {
+      currentIndex = (currentIndex - 1 + $cards.length) % $cards.length;
+      updateCarousel();
+    });
+  
+    updateCarousel(); // Initial call
+
+  });
+  
